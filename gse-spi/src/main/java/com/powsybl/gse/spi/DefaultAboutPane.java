5a07bf7ea14139ba3be7221b11f2f2cc59988bfc
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2018-08-02 13:19:11 +0200

    Next

--- gse-spi/src/main/java/com/powsybl/gse/spi/DefaultAboutPane.java
+++ gse-spi/src/main/java/com/powsybl/gse/spi/DefaultAboutPane.java
@@ -7,55 +7,80 @@
 package com.powsybl.gse.spi;
 
 import com.powsybl.tools.Version;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import org.apache.commons.lang3.SystemUtils;
+import org.joda.time.DateTime;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public class DefaultAboutPane extends BorderPane {
 
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("lang.DefaultAboutPane");
+
     protected static final double LOGO_SIZE = 128;
+
+    protected static final double PADDING = 5;
+
+    protected final VBox main;
 
     protected List<Node> getAdditionalLogos() {
         return Collections.emptyList();
     }
 
+    @Deprecated
+    /**
+     * @deprecated directly add additional components to main pane.
+     */
     protected List<String> getAdditionalInfos() {
         return Collections.emptyList();
     }
 
     public DefaultAboutPane() {
         setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-//        setPrefSize(600, 400);
+        setPrefWidth(600);
 
         PowsyblLogo powsyblLogo = new PowsyblLogo(LOGO_SIZE, LOGO_SIZE);
 
-        HBox logoPane = new HBox(5, powsyblLogo);
+        HBox logoPane = new HBox(PADDING, powsyblLogo);
         logoPane.getChildren().addAll(getAdditionalLogos());
-        logoPane.setPadding(new Insets(5, 5, 5, 5));
+        logoPane.setPadding(new Insets(PADDING, PADDING, PADDING, PADDING));
         setTop(logoPane);
-        TextArea infos = new TextArea();
-        infos.setStyle("-fx-font-family:monospace;-fx-font-size:18px");
-        infos.setEditable(false);
-        infos.setPrefRowCount(20);
-        infos.setText(String.join(System.lineSeparator() + System.lineSeparator(),
-                                  "Java version: " + SystemUtils.JAVA_VERSION,
-                                  Version.getTableString()) +
-                      System.lineSeparator() + System.lineSeparator() +
-                      String.join(System.lineSeparator() + System.lineSeparator(), getAdditionalInfos()));
-        setCenter(new ScrollPane(infos));
+
+        main = new VBox(PADDING,
+                        new Label(RESOURCE_BUNDLE.getString("JavaVersion") + ": " + SystemUtils.JAVA_VERSION),
+                        new Label(RESOURCE_BUNDLE.getString("RepositoryVersions") + ": "),
+                        createVersionTable());
+        main.setPadding(new Insets(PADDING, PADDING, PADDING, PADDING));
+        setCenter(main);
+    }
+
+    private static TableView<Version> createVersionTable() {
+        TableView<Version> versionTable = new TableView<>();
+        TableColumn<Version, String> repositoryNameCol = new TableColumn<>(RESOURCE_BUNDLE.getString("RepositoryName"));
+        repositoryNameCol.setCellValueFactory(new PropertyValueFactory<>("repositoryName"));
+        TableColumn<Version, String> mavenProjectVersionCol = new TableColumn<>(RESOURCE_BUNDLE.getString("MavenProjectVersion"));
+        mavenProjectVersionCol.setCellValueFactory(new PropertyValueFactory<>("mavenProjectVersion"));
+        TableColumn<Version, String> gitVersionCol = new TableColumn<>(RESOURCE_BUNDLE.getString("GitVersion"));
+        gitVersionCol.setCellValueFactory(new PropertyValueFactory<>("gitVersion"));
+        TableColumn<Version, String> gitBranchCol = new TableColumn<>(RESOURCE_BUNDLE.getString("GitBranch"));
+        gitBranchCol.setCellValueFactory(new PropertyValueFactory<>("gitBranch"));
+        TableColumn<Version, String> buildTimestampCol = new TableColumn<>(RESOURCE_BUNDLE.getString("BuildTimestamp"));
+        buildTimestampCol.setCellValueFactory(param -> new SimpleStringProperty(new DateTime(param.getValue().getBuildTimestamp()).toString()));
+        versionTable.getColumns().addAll(repositoryNameCol, mavenProjectVersionCol, gitVersionCol, gitBranchCol, buildTimestampCol);
+        versionTable.getItems().addAll(Version.list());
+        return versionTable;
     }
 }
