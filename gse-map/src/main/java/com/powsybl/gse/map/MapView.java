132ede1084fd46079cdeba9ea4ef5dc6da107c37
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2018-09-11 21:15:32 +0200

    Cleanup

--- gse-map/src/main/java/com/powsybl/gse/map/MapView.java
+++ gse-map/src/main/java/com/powsybl/gse/map/MapView.java
@@ -121,25 +121,22 @@ public class MapView extends Region {
         double tileWidth = tileManager.getServerInfo().getTileWidth();
         double tileHeight = tileManager.getServerInfo().getTileHeight();
 
-        // draw center tile
-        double x = getWidth() / 2 - tileWidth * (tilePoint.getX() - tile.getX());
-        double y = getHeight() / 2 - tileHeight * (tilePoint.getY() - tile.getY());
-        drawTile(tile, x, y);
+        // compute center tile screen point
+        double xScreen = getWidth() / 2 - tileWidth * (tilePoint.getX() - tile.getX());
+        double yScreen = getHeight() / 2 - tileHeight * (tilePoint.getY() - tile.getY());
 
         // compute number of tiles needed to fill the screen
-        int n1 = (int) Math.ceil(x / tileWidth);
-        int n2 = (int) Math.ceil((getWidth() - x - tileWidth) / tileWidth);
-        int m1 = (int) Math.ceil(y / tileHeight);
-        int m2 = (int) Math.ceil((getHeight() - y - tileHeight) / tileHeight);
+        int n1 = (int) Math.ceil(xScreen / tileWidth);
+        int n2 = (int) Math.ceil((getWidth() - xScreen - tileWidth) / tileWidth);
+        int m1 = (int) Math.ceil(yScreen / tileHeight);
+        int m2 = (int) Math.ceil((getHeight() - yScreen - tileHeight) / tileHeight);
 
-        // draw other tiles
+        // draw tiles
         for (int i = -n1; i <= n2; i++) {
             for (int j = -m1; j <= m2; j++) {
-                if (i != 0 || j != 0) {
-                    drawTile(new Tile(tile.getX() + i, tile.getY() + j, zoom.get(), tileManager),
-                             x + i * tileWidth,
-                             y + j * tileHeight);
-                }
+                drawTile(new Tile(tile.getX() + i, tile.getY() + j, zoom.get(), tileManager),
+                         xScreen + i * tileWidth,
+                         yScreen + j * tileHeight);
             }
         }
     }
