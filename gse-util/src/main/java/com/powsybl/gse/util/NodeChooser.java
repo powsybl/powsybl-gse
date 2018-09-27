74c04ce20e52a9e96c048299384859bbdcb8c547
Nassirou Nambiena <nassirou.nambiena@rte-france.com>
2018-09-27 17:53:35 +0200

    correct Nodechooser conflict

--- gse-util/src/main/java/com/powsybl/gse/util/NodeChooser.java
+++ gse-util/src/main/java/com/powsybl/gse/util/NodeChooser.java
@@ -112,11 +112,12 @@ public class NodeChooser<N, F extends N, D extends N, T extends N> extends GridP
                                 setText(treeModel.getName(item));
                                 setTextFill(Color.BLACK);
                                 setOpacity(item.getClass() == treeModel.getUnknownFileClass() ? 0.5 : 1);
+                                setGraphic(NodeGraphics.getGraphic(item));
                                 setOnDragDetected(event -> dragDetectedEvent(getItem(), getTreeTableRow().getTreeItem(), event));
                                 setOnDragOver(this::dragOverEvent);
                                 setOnDragDropped(event -> dragDroppedEvent(getItem(), getTreeTableRow().getTreeItem(), event, node));
                                 setOnDragExited(event -> setTextFill(Color.BLACK));
-                            } else{
+                            } else {
                                 setText(treeModel.getName(item));
                                 setTextFill(Color.BLACK);
                                 setOpacity(item.getClass() == treeModel.getUnknownFileClass() ? 0.5 : 1);
