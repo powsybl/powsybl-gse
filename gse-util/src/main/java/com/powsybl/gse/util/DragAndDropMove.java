02edb8002cbda9e891dacd1af0c0cac16f8b0b8c
Nassirou Nambiena <nassirou.nambiena@rte-france.com>
2018-10-24 18:13:06 +0200

    change MoveContext design

---
+++ gse-util/src/main/java/com/powsybl/gse/util/DragAndDropMove.java
@@ -0,0 +1,28 @@
+package com.powsybl.gse.util;
+
+import javafx.scene.control.TreeItem;
+
+public class DragAndDropMove {
+    private Object source;
+    private TreeItem sourceTreeItem;
+
+    public DragAndDropMove() {
+
+    }
+
+    public Object getSource() {
+        return source;
+    }
+
+    public void setSource(Object source) {
+        this.source = source;
+    }
+
+    public TreeItem getSourceTreeItem() {
+        return sourceTreeItem;
+    }
+
+    public void setSourceTreeItem(TreeItem sourceTreeItem) {
+        this.sourceTreeItem = sourceTreeItem;
+    }
+}
