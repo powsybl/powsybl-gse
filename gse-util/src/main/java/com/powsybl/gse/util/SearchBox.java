d20584212bc26645918ab932c1763af9b5703d3a
Nassirou Nambiena <nassirou.nambiena@rte-france.com>
2018-12-18 10:08:55 +0100

    change label font

--- gse-util/src/main/java/com/powsybl/gse/util/SearchBox.java
+++ gse-util/src/main/java/com/powsybl/gse/util/SearchBox.java
@@ -11,14 +11,13 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import org.controlsfx.control.textfield.CustomTextField;
 import org.controlsfx.control.textfield.TextFields;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -26,7 +25,9 @@ import java.util.stream.Collectors;
  */
 public class SearchBox extends VBox {
 
-    private Label label = new Label("Enter a file name");
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("lang.SearchBox");
+
+    private Label label = new Label(RESOURCE_BUNDLE.getString("EnterFileName"));
 
     private CustomTextField searchField = (CustomTextField) TextFields.createClearableTextField();
 
@@ -38,6 +39,7 @@ public class SearchBox extends VBox {
         setPrefSize(460, 30);
         searchField.setLeft(searchGlyph);
         searchField.setPrefWidth(450);
+        label.setFont(Font.font("verdana", FontWeight.BLACK,13));;
         getChildren().addAll(label, searchField);
         this.contextMenu = contextMenuArg;
         Set<MenuItem> menuItems = new HashSet<>(contextMenu.getItems());
