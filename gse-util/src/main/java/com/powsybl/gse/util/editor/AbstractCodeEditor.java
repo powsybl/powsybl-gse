611f5581203b7b484747aa71fd72404b324a2e41
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-11-07 15:36:01 +0100

    Fix licence header
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- gse-util/src/main/java/com/powsybl/gse/util/editor/AbstractCodeEditor.java
+++ gse-util/src/main/java/com/powsybl/gse/util/editor/AbstractCodeEditor.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
 package com.powsybl.gse.util.editor;
 
 import javafx.beans.value.ObservableValue;
@@ -5,6 +12,9 @@ import org.controlsfx.control.MasterDetailPane;
 
 import java.util.List;
 
+/**
+ * @author Paul Bui-Quang <paul.buiquang at rte-france.com>
+ */
 public abstract class AbstractCodeEditor extends MasterDetailPane {
 
     public abstract void setTabSize(int size);
